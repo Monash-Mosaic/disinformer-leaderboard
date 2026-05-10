73d2e3ec29fcb40960691e32e24800c733497405
Raveesh Yadav <raveeshyadav8@outlook.com>
2026-05-10 10:17:36 +1000

    re-enable home links

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  return redirect("/leaderboard-offsetbased");
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#ffffef] font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold">Home</h1>
+    </div>
+  );
 }
