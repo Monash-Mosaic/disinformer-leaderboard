557bb48740e9c004f2f10c3a83eca035fb1f2d0f
Raveesh Yadav <raveeshyadav8@outlook.com>
2026-06-07 12:13:56 +1000

    feat: home roles section

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import HomeCarouselSection from "@/components/home/HomeCarouselSection";
 import HomeGameInfoSection from "@/components/home/HomeGameInfoSection";
+import HomeGameRoleSection from "@/components/home/HomeGameRoleSection";
 import HomeHeroSection from "@/components/home/HomeHeroSection";
 
 export default function HomePage() {
@@ -7,6 +8,7 @@ export default function HomePage() {
     <main className="min-h-[calc(100vh-12rem)] bg-[#ffffef] text-[#2d4143]">
       <HomeHeroSection />
       <HomeGameInfoSection />
+      <HomeGameRoleSection />
       <HomeCarouselSection />
     </main>
   );
